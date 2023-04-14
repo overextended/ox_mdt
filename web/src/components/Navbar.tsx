1175a4695ee9a18d0ab7cb9c79a244bf98aa10f5
LukeWasTaken <39926192+LukeWasTakenn@users.noreply.github.com>
2023-04-14 20:08:14 +0200

    feat: officer name at bottom of nav

--- web/src/components/Navbar.tsx
+++ web/src/components/Navbar.tsx
@@ -13,15 +13,24 @@ import NavButton from './NavButton';
 
 export const Navbar: Component = () => {
   return (
-    <aside class="flex h-full w-1/4 flex-col items-center rounded-bl-md rounded-tl-md bg-durple-400 p-4 font-main text-white">
-      <p class="self-start py-2 text-xs font-bold uppercase text-dark-200">Pages</p>
-      <NavButton icon={IconLayoutDashboard} label="Dashboard" path="/" />
-      <NavButton icon={IconUsers} label="Profiles" path="/profiles" />
-      <NavButton icon={IconReceipt} label="Reports" path="/reports" />
-      <NavButton icon={IconMap2} label="Dispatch" path="/dispatch" />
-      <NavButton icon={IconGavel} label="Laws" path="/laws" />
-      <p class="self-start py-2 text-xs font-bold uppercase text-dark-200">Management</p>
-      <NavButton icon={IconUserShield} label="Officers" path="/manage" />
+    <aside class="flex h-full w-1/4 flex-col justify-between rounded-bl-md rounded-tl-md bg-durple-400 p-4 font-main text-white">
+      <div>
+        <p class="self-start py-2 text-xs font-bold uppercase text-dark-200">Pages</p>
+        <NavButton icon={IconLayoutDashboard} label="Dashboard" path="/" />
+        <NavButton icon={IconUsers} label="Profiles" path="/profiles" />
+        <NavButton icon={IconReceipt} label="Reports" path="/reports" />
+        <NavButton icon={IconMap2} label="Dispatch" path="/dispatch" />
+        <NavButton icon={IconGavel} label="Laws" path="/laws" />
+        <p class="self-start py-2 text-xs font-bold uppercase text-dark-200">Management</p>
+        <NavButton icon={IconUserShield} label="Officers" path="/officers" />
+      </div>
+      <div class="flex w-full items-center gap-4 rounded-md bg-durple-200 p-2">
+        <img src="https://avatars.githubusercontent.com/u/39926192?s=120&v=4" alt="pp" class="h-10 w-10 rounded-full" />
+        <div class="flex flex-col justify-center overflow-hidden">
+          <p class="truncate text-sm text-dark-50">Svetozar Miletić</p>
+          <p class="text-xs text-dark-200">LSPD Officer</p>
+        </div>
+      </div>
     </aside>
   );
 };
