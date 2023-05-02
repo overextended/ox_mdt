6a66fe7e5088a43c62c23d8143c68f153d7bd2c1
LukeWasTaken <39926192+LukeWasTakenn@users.noreply.github.com>
2023-05-02 15:12:15 +0200

    refactor(web/theme): edit select and checkbox to match the theme

--- web/src/theme.ts
+++ web/src/theme.ts
@@ -7,6 +7,27 @@ export const theme: MantineThemeOverride = {
     md: '0 4px 3px rgba(0, 0, 0, 0.07)',
   },
   components: {
+    Checkbox: {
+      styles: (theme) => ({
+        input: {
+          backgroundColor: theme.colors.durple[4],
+          borderColor: theme.colors.durple[2],
+        },
+      }),
+    },
+    Select: {
+      styles: (theme) => ({
+        dropdown: {
+          backgroundColor: theme.colors.durple[4],
+          borderColor: 'transparent',
+        },
+        item: {
+          '&:hover': {
+            backgroundColor: theme.colors.durple[2],
+          },
+        },
+      }),
+    },
     Badge: {
       defaultProps: { variant: 'default' },
       variants: {
