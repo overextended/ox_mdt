a48ff9c2780e6ef673b6a9f1fef2137b53e48a7e
LukeWasTaken <39926192+LukeWasTakenn@users.noreply.github.com>
2023-05-10 11:52:05 +0200

    feat(web/reports): ability to remove evidence

--- web/src/pages/reports/components/ReportEvidence.tsx
+++ web/src/pages/reports/components/ReportEvidence.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { ActionIcon, Badge, Button, Group, rem, Text } from '@mantine/core';
 import { IconEdit, IconPaperBag, IconX } from '@tabler/icons-react';
-import { useActiveReport, useEvidence } from '../../../state';
+import { useActiveReport, useEvidence, useSetEvidence } from '../../../state';
 import BadgeButton from '../../../components/BadgeButton';
 import { modals } from '@mantine/modals';
 import AddEvidenceModal from './modals/AddEvidenceModal';
 
 const ReportEvidence: React.FC = () => {
   const evidence = useEvidence();
+  const setEvidence = useSetEvidence();
 
   return (
     <>
@@ -20,11 +21,32 @@ const ReportEvidence: React.FC = () => {
           label="Add evidence"
           onClick={() => modals.open({ title: 'Add evidence', children: <AddEvidenceModal />, size: 'sm' })}
         />
-        {evidence.map((evidence) => (
+        {evidence.map((evidence, index) => (
           <Badge
             key={evidence.type === 'item' ? evidence.item : evidence.url}
             rightSection={
-              <ActionIcon size="xs" radius="xl" variant="transparent">
+              <ActionIcon
+                size="xs"
+                radius="xl"
+                variant="transparent"
+                onClick={() =>
+                  modals.openConfirmModal({
+                    title: 'Remove evidence',
+                    children: (
+                      <Text size="sm">
+                        Are you sure you want to remove{' '}
+                        {evidence.type === 'image' ? evidence.label : `${evidence.count}x ${evidence.item}`} from
+                        evidence?
+                      </Text>
+                    ),
+                    labels: { confirm: 'Confirm', cancel: 'Cancel' },
+                    confirmProps: { color: 'red' },
+                    onConfirm: () => {
+                      setEvidence((prev) => prev.filter((_, indx) => indx !== index));
+                    },
+                  })
+                }
+              >
                 <IconX size={rem(10)} />
               </ActionIcon>
             }
