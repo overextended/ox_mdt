7b309fa35ff3da5320d5a1dc1d957d00966f6d03
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2023-07-01 11:05:35 +0200

    refactor(web/nav): use mantine's light variant colors for nav buttons

--- web/src/components/NavButton.tsx
+++ web/src/components/NavButton.tsx
@@ -2,18 +2,22 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { createStyles, Group, Stack, Text, Tooltip, UnstyledButton } from '@mantine/core';
 
-const useStyles = createStyles((theme, params: { isSmall?: boolean; isActive?: boolean }) => ({
-  button: {
-    color: params.isActive ? theme.colors.blue[4] : theme.colors.dark[0],
-    backgroundColor: params.isActive ? theme.fn.rgba(theme.colors.blue[6], 0.2) : undefined,
-    padding: !params.isSmall ? theme.spacing.md : 8,
-    borderRadius: theme.radius.md,
-    fontWeight: 500,
-    transition: '150ms',
-    width: params.isSmall ? 50 : undefined,
-    '&:hover': !params.isActive && { backgroundColor: theme.colors.durple[2], color: 'white' },
-  },
-}));
+const useStyles = createStyles((theme, params: { isSmall?: boolean; isActive?: boolean }) => {
+  const colors = theme.fn.variant({ color: 'blue', variant: 'light' });
+
+  return {
+    button: {
+      color: params.isActive ? colors.color : theme.colors.dark[0],
+      backgroundColor: params.isActive ? colors.background : undefined,
+      padding: !params.isSmall ? theme.spacing.md : 8,
+      borderRadius: theme.radius.md,
+      fontWeight: 500,
+      transition: '150ms',
+      width: params.isSmall ? 50 : undefined,
+      '&:hover': !params.isActive && { backgroundColor: theme.colors.durple[2], color: 'white' },
+    },
+  };
+});
 
 const NavButton: React.FC<{
   icon: React.ComponentType;
