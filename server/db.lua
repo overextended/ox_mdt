ef99d23f231feb353698c39b75d93f2f8389b75d
Linden <65407488+thelindat@users.noreply.github.com>
2023-07-04 10:27:25 +1000

    refactor(server/db): use prepared statements

--- server/db.lua
+++ server/db.lua
@@ -33,14 +33,15 @@ MySQL.transaction({
 })
 
 local db = {}
-local selectCharacter = 'SELECT `firstName`, `lastName`, DATE_FORMAT(`dateofbirth`, "%Y-%m-%d") as dob, `charid` as id FROM `characters`'
+local selectCharacter =
+'SELECT `firstName`, `lastName`, DATE_FORMAT(`dateofbirth`, "%Y-%m-%d") as dob, `charid` as id FROM `characters`'
 local wildcard = '%s%%'
 
 local selectCharacterById = selectCharacter .. ' WHERE `charid` LIKE ?'
 
 ---@param id number | string
 function db.selectCharacterById(id)
-    return MySQL.query.await(selectCharacterById, { wildcard:format(id) })
+    return MySQL.rawExecute.await(selectCharacterById, { wildcard:format(id) })
 end
 
 local selectCharacterByNameA = selectCharacter .. ' WHERE `firstName` LIKE ? OR `lastName` LIKE ?'
@@ -53,30 +54,29 @@ function db.selectCharacterByName(name)
     if nameB == '' then
         nameA = wildcard:format(nameA)
 
-        return MySQL.query.await(selectCharacterByNameA, { nameA, nameA })
+        return MySQL.rawExecute.await(selectCharacterByNameA, { nameA, nameA })
     end
 
-    return MySQL.query.await(selectCharacterByNameB, { nameA, wildcard:format(nameB) })
+    return MySQL.rawExecute.await(selectCharacterByNameB, { nameA, wildcard:format(nameB) })
 end
 
 ---@param title string
 ---@param author string
 function db.createReport(title, author)
-    return MySQL.insert.await('INSERT INTO `ox_mdt_reports` (`title`, `author`) VALUES (?, ?)', { title, author })
+    return MySQL.prepare.await('INSERT INTO `ox_mdt_reports` (`title`, `author`) VALUES (?, ?)', { title, author })
 end
 
 ---@param id number
 function db.selectReportById(id)
-    return MySQL.single.await('SELECT `id`, `title`, `description` FROM `ox_mdt_reports` WHERE `id` = ?', { id })
+    return MySQL.prepare.await('SELECT `id`, `title`, `description` FROM `ox_mdt_reports` WHERE `id` = ? LIMIT 1', { id })
 end
-
 
 local selectReports = 'SELECT `id`, `title`, `author`, `date` FROM `ox_mdt_reports`'
 local selectReportsById = selectReports .. 'WHERE `id` LIKE ?'
 
 ---@param id number | string
 function db.selectReportsById(id)
-    return MySQL.query.await(selectReportsById, { wildcard:format(id) })
+    return MySQL.rawExecute.await(selectReportsById, { wildcard:format(id) })
 end
 
 local selectReportsByString = selectReports .. ' WHERE `title` LIKE ? or `author` LIKE ? or `date` LIKE ?'
@@ -84,22 +84,25 @@ local selectReportsByString = selectReports .. ' WHERE `title` LIKE ? or `author
 ---@param search string
 function db.selectReports(search)
     search = wildcard:format(search)
-    return MySQL.query.await(selectReportsByString, { search })
+    return MySQL.rawExecute.await(selectReportsByString, { search })
 end
 
 ---@param id number
 function db.deleteReport(id)
-    return MySQL.update.await('DELETE FROM `ox_mdt_reports` WHERE `id` = ?', { id })
+    return MySQL.prepare.await('DELETE FROM `ox_mdt_reports` WHERE `id` = ?', { id })
 end
 
 ---@param title string
 ---@param reportId number
 function db.updateReportTitle(title, reportId)
-    return MySQL.update.await('UPDATE `ox_mdt_reports` SET `title` = ? WHERE `id` = ?', { title, reportId })
+    return MySQL.prepare.await('UPDATE `ox_mdt_reports` SET `title` = ? WHERE `id` = ?', { title, reportId })
 end
 
-function db.selectProfiles(search)
-    return MySQL.query.await('SELECT `charid` AS playerId, `firstname` AS firstName, `lastname` as lastName, `dateofbirth` AS dob FROM `characters`')
+---@return ProfileCard[]
+function db.selectProfiles()
+    return MySQL.query.await(
+        'SELECT `charid` AS playerId, `firstName`, `lastName`, `dateofbirth` AS dob FROM `characters`')
+end
 end
 
 
