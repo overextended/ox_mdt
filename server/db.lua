d1f39e5c677dea7e03070413f1f4598fcdf74d5c
Linden <65407488+thelindat@users.noreply.github.com>
2023-07-04 10:25:21 +1000

    feat(server/db): additional mdt tables

--- server/db.lua
+++ server/db.lua
@@ -1,3 +1,37 @@
+MySQL.transaction({
+    [[
+        CREATE TABLE IF NOT EXISTS `ox_mdt_reports` (
+            `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT,
+            `title` VARCHAR(50) NOT NULL,
+            `description` TEXT NULL DEFAULT NULL,
+            `author` VARCHAR(50) NULL DEFAULT NULL,
+            `date` DATETIME NULL DEFAULT curtime(),
+            PRIMARY KEY (`id`) USING BTREE
+        ) ENGINE=InnoDB;
+    ]],
+
+    [[
+        CREATE TABLE IF NOT EXISTS `ox_mdt_reports_characters` (
+            `reportid` INT(10) UNSIGNED NOT NULL,
+            `charid` INT(10) UNSIGNED NOT NULL,
+            `type` TINYINT(3) UNSIGNED NOT NULL,
+            INDEX `FK_ox_mdt_reports_officers_characters` (`charid`) USING BTREE,
+            INDEX `reportid` (`reportid`) USING BTREE,
+            CONSTRAINT `FK_ox_mdt_reports_officers_characters` FOREIGN KEY (`charid`) REFERENCES `characters` (`charid`) ON UPDATE CASCADE ON DELETE CASCADE,
+            CONSTRAINT `FK_ox_mdt_reports_officers_ox_mdt_reports` FOREIGN KEY (`reportid`) REFERENCES `ox_mdt_reports` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
+        ) ENGINE=InnoDB;        
+    ]],
+
+    [[
+        CREATE TABLE IF NOT EXISTS `ox_mdt_charges` (
+            `charid` INT(10) UNSIGNED NOT NULL,
+            `charge` VARCHAR(50) NOT NULL,
+            INDEX `FK_ox_mdt_charges_characters` (`charid`) USING BTREE,
+            CONSTRAINT `FK_ox_mdt_charges_characters` FOREIGN KEY (`charid`) REFERENCES `characters` (`charid`) ON UPDATE CASCADE ON DELETE CASCADE
+        ) ENGINE=InnoDB;        
+    ]],
+})
+
 local db = {}
 local selectCharacter = 'SELECT `firstName`, `lastName`, DATE_FORMAT(`dateofbirth`, "%Y-%m-%d") as dob, `charid` as id FROM `characters`'
 local wildcard = '%s%%'
