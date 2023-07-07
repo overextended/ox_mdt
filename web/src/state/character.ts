72510b1d21f4f1223970d6fd6ae4a44b2aced4fc
Luke <39926192+LukeWasTakenn@users.noreply.github.com>
2023-07-07 11:55:10 +0200

    chore(web): use debug character when in browser

--- web/src/state/character.ts
+++ web/src/state/character.ts
@@ -1,14 +1,28 @@
 import { atom, useAtom, useAtomValue, useSetAtom } from 'jotai';
 import { Character } from '../typings';
+import { isEnvBrowser } from '../utils/misc';
 
-const characterAtom = atom<Character>({
-  stateId: 199351,
-  firstName: '',
-  lastName: '',
-  title: '',
-  grade: 0,
-  callSign: 0,
-});
+const DEBUG_CHARACTER: Character = {
+  stateId: 1993201,
+  firstName: 'Svetozar',
+  lastName: 'Miletić',
+  title: 'LSPD Officer',
+  grade: 4,
+  callSign: 103,
+};
+
+const characterAtom = atom<Character>(
+  isEnvBrowser()
+    ? DEBUG_CHARACTER
+    : {
+        stateId: 0,
+        firstName: '',
+        lastName: '',
+        title: '',
+        grade: 0,
+        callSign: 0,
+      }
+);
 
 export const useCharacter = () => useAtomValue(characterAtom);
 export const useSetCharacter = () => useSetAtom(characterAtom);
