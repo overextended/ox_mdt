f2db1b9af4455e1989263cebaf50dbafc134a174
Linden <65407488+thelindat@users.noreply.github.com>
2023-07-18 18:04:59 +1000

    refactor(web/locales): display key for isEnvBrowser or undefined locales

--- web/src/locales.ts
+++ web/src/locales.ts
@@ -4,14 +4,16 @@ import locale from '../../locales/en.json';
 const locales: Record<string, string | number> = {};
 
 export function setLocale(data: typeof locale) {
-  for (const key in locales) delete locales[key];
+  for (const key in locales) locales[key] = key;
   for (const [key, value] of Object.entries(data)) {
     locales[key] = value;
   }
 }
 
 if (isEnvBrowser()) {
-  setLocale(locale);
+  for (const [key] of Object.entries(locale)) {
+    locales[key] = key;
+  }
 }
 
 export default locales as typeof locale;
