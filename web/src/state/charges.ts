6e6cbd9aaf31e81faaeaaa822ff2c6145aae736b
Linden <65407488+thelindat@users.noreply.github.com>
2023-08-28 08:03:07 +1000

    fix(web/charges): update dummy data

--- web/src/state/charges.ts
+++ web/src/state/charges.ts
@@ -6,29 +6,23 @@ const CHARGES: Charge[] = [
     label: 'Robbery of a finanical institution',
     description: 'Bank robbery go brrr',
     type: 'felony',
-    penalty: {
-      time: 30,
-      fine: 3000,
-    },
+    time: 30,
+    fine: 3000,
   },
   {
     label: 'Speeding',
     description:
       'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, doloribus eveniet facere ipsam, ipsum minus modi molestiae nesciunt odio saepe sapiente sed sint voluptatibus voluptatum!',
     type: 'infraction',
-    penalty: {
-      time: 0,
-      fine: 2500,
-    },
+    time: 0,
+    fine: 2500,
   },
   {
     label: 'Loitering',
     description: 'Standing go brrr',
-    type: 'misdemeanour',
-    penalty: {
-      time: 90,
-      fine: 25000,
-    },
+    type: 'misdemeanor',
+    time: 90,
+    fine: 25000,
   },
 ];
 
