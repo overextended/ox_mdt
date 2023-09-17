bcb0f0f25327d70017eb1b3286619c58d7377b7b
Linden <65407488+thelindat@users.noreply.github.com>
2023-09-17 23:26:23 +1000

    refactor(server/dbSearch): search string creation

--- server/dbSearch.lua
+++ server/dbSearch.lua
@@ -1,35 +1,26 @@
-local wildcard = '%s%%'
-
----Split a search string into search terms for partial and exact lookup by name.
----@param search string
----@return string
----@return string
----@return boolean?
-local function splitSearch(search)
-    local a, b = search:match('^([%w]+) ?([%w]*)$')
-
-    if b == '' then
-        a = wildcard:format(a)
-
-        return a, a
-    end
-
-    return a, wildcard:format(b), true
-end
-
 ---@generic T
 ---@param fn async fun(parameters?: table, match?: boolean): T
 ---@param search string?
 ---@param offset number? Offset query results when using LIMIT.
----@return T
+---@return T?
 local function dbSearch(fn, search, offset)
     if not search or search == '' then
         return fn({ offset })
     end
 
-    local a, b, match = splitSearch(search)
+    local str = {}
 
-    return fn({ a, b, offset }, match or false)
+    for word in search:gmatch('%S+') do
+        str[#str + 1] = '+'
+        str[#str + 1] = word:gsub('[%p%c]', '')
+        str[#str + 1] = '*'
+    end
+
+    if #str > 3 then table.remove(str, 3) end
+
+    search = table.concat(str)
+
+    return fn({ search == '+*' and '' or search, offset }, true)
 end
 
 return dbSearch
