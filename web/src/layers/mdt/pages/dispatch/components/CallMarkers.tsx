5d78bd3e44a4bf6480e6f5d72f08dcb57bd90fa3
Ceeb <cyril.beeckman@gmail.com>
2024-07-19 11:03:42 +0200

    feat(map): complete refacto to use tiles and rockstar cdn

--- web/src/layers/mdt/pages/dispatch/components/CallMarkers.tsx
+++ web/src/layers/mdt/pages/dispatch/components/CallMarkers.tsx
@@ -7,39 +7,40 @@ import MarkerPopup from './MarkerPopup';
 import { useNuiEvent } from '../../../../../hooks/useNuiEvent';
 import { queryClient } from '../../../../../main';
 import { Call } from '../../../../../typings';
+import { gameToMap } from './Map';
 
 const CallMarkers: React.FC = () => {
-  const calls = useCalls();
+	const calls = useCalls();
 
-  useNuiEvent('updateCallCoords', (data: { id: number; coords: [number, number] }) => {
-    queryClient.setQueriesData(['calls'], (oldData: Call[] | undefined) => {
-      if (!oldData) return;
+	useNuiEvent('updateCallCoords', (data: { id: number; coords: [number, number] }) => {
+		queryClient.setQueriesData(['calls'], (oldData: Call[] | undefined) => {
+			if (!oldData) return;
 
-      return oldData.map((prevCall) => (prevCall.id === data.id ? { ...prevCall, coords: data.coords } : prevCall));
-    });
-  });
+			return oldData.map((prevCall) => (prevCall.id === data.id ? { ...prevCall, coords: data.coords } : prevCall));
+		});
+	});
 
-  return (
-    <>
-      {calls.length > 0 &&
-        calls.map((call) => (
-          <Marker
-            key={call.id}
-            position={[call.coords[1], call.coords[0]]}
-            icon={L.icon({
-              iconSize: [26, 26],
-              iconUrl: `./blips/${call.blip}.png`,
-            })}
-          >
-            <MarkerPopup>
-              <Text>
-                {call.offense} ({call.code})
-              </Text>
-            </MarkerPopup>
-          </Marker>
-        ))}
-    </>
-  );
+	return (
+		<>
+			{calls.length > 0 &&
+				calls.map((call) => (
+					<Marker
+						key={call.id}
+						position={gameToMap(call.coords[0], call.coords[1])}
+						icon={L.icon({
+							iconSize: [26, 26],
+							iconUrl: `./blips/${call.blip}.png`,
+						})}
+					>
+						<MarkerPopup>
+							<Text>
+								{call.offense} ({call.code})
+							</Text>
+						</MarkerPopup>
+					</Marker>
+				))}
+		</>
+	);
 };
 
 export default CallMarkers;
--- web/src/layers/mdt/pages/dispatch/components/OfficerMarkers.tsx
+++ web/src/layers/mdt/pages/dispatch/components/OfficerMarkers.tsx
@@ -8,68 +8,69 @@ import { IconCircleFilled } from '@tabler/icons-react';
 import L from 'leaflet';
 import MarkerPopup from './MarkerPopup';
 import { Officer } from '../../../../../typings';
+import { gameToMap } from './Map';
 
 const useStyles = createStyles((theme) => ({
-  icon: {
-    color: theme.colors.blue[8],
-  },
+	icon: {
+		color: theme.colors.blue[8],
+	},
 }));
 
 const OfficerMarkers: React.FC = () => {
-  const [officers, setOfficers] = React.useState<Officer[]>([]);
-  const { classes } = useStyles();
+	const [officers, setOfficers] = React.useState<Officer[]>([]);
+	const { classes } = useStyles();
 
-  debugData<Officer[]>([
-    {
-      data: [
-        { firstName: 'John', lastName: 'Snow', position: [0, 0, 0], stateId: '391231', playerId: 1 },
-        {
-          firstName: 'Billy',
-          lastName: 'Bob',
-          position: [250, 250, 250],
-          callSign: 564,
-          stateId: '312351',
-          playerId: 2,
-        },
-        {
-          firstName: 'Merry',
-          lastName: 'Jane',
-          position: [1000, 300, 0],
-          callSign: 751,
-          stateId: '103214',
-          playerId: 3,
-        },
-      ],
-      action: 'updateOfficerPositions',
-    },
-  ]);
+	debugData<Officer[]>([
+		{
+			data: [
+				{ firstName: 'John', lastName: 'Snow', position: [0, 0, 0], stateId: '391231', playerId: 1 },
+				{
+					firstName: 'Billy',
+					lastName: 'Bob',
+					position: [250, 250, 250],
+					callSign: 564,
+					stateId: '312351',
+					playerId: 2,
+				},
+				{
+					firstName: 'Merry',
+					lastName: 'Jane',
+					position: [300, 1000, 0],
+					callSign: 751,
+					stateId: '103214',
+					playerId: 3,
+				},
+			],
+			action: 'updateOfficerPositions',
+		},
+	]);
 
-  useNuiEvent<Officer[]>('updateOfficerPositions', (data) => {
-    setOfficers(data);
-  });
+	useNuiEvent<Officer[]>('updateOfficerPositions', (data) => {
+		setOfficers(data);
+	});
 
-  return (
-    <>
-      {officers.length > 0 &&
-        officers.map((officer) => (
-          <Marker
-            key={officer.stateId}
-            position={[officer.position[0], officer.position[1]]}
-            icon={L.divIcon({
-              className: 'custom-icon',
-              iconSize: [20, 20],
-              html: ReactDOMServer.renderToString(<IconCircleFilled size={20} className={classes.icon} />),
-            })}
-          >
-            <MarkerPopup>
-              <Text>
-                {officer.firstName} {officer.lastName} {officer.callSign ? `(${officer.callSign})` : ''}
-              </Text>
-            </MarkerPopup>
-          </Marker>
-        ))}
-    </>
-  );
+	return (
+		<>
+			{officers.length > 0 &&
+				officers.map((officer) => (
+					<Marker
+						key={officer.stateId}
+						position={gameToMap(officer.position[0], officer.position[1])}
+						icon={L.divIcon({
+							className: 'custom-icon',
+							iconSize: [20, 20],
+							html: ReactDOMServer.renderToString(<IconCircleFilled size={20} className={classes.icon} />),
+						})}
+					>
+						<MarkerPopup>
+							<Text>
+								{officer.firstName} {officer.lastName} {officer.callSign ? `(${officer.callSign})` : ''}
+							</Text>
+						</MarkerPopup>
+					</Marker>
+				))}
+		</>
+	);
 };
 
 export default OfficerMarkers;
