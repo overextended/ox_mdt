9c1a9d0bf876f01e62ea3a7615da1c9a0d7a836f
Linden <65407488+thelindat@users.noreply.github.com>
2025-03-12 11:58:33 +1100

    refactor(server/isAuthorised): only pass permission string

--- server/utils/isAuthorised.lua
+++ server/utils/isAuthorised.lua
@@ -1,16 +1,15 @@
 local config = require 'config'
-local permissions = require 'server.permissions'
 local framework
 
 CreateThread(function()
-   framework = require(('server.framework.%s'):format(config.framework))
+    framework = require(('server.framework.%s'):format(config.framework))
 end)
 
 ---@param playerId number
 ---@param permission string
 ---@return boolean?
 local function isAuthorised(playerId, permission)
-    return framework.isAuthorised(playerId, permissions[permission] or 0, permission)
+    return framework.isAuthorised(playerId, permission)
 end
 
 return isAuthorised
