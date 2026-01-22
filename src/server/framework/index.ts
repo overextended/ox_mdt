ea48ec14e76a4743cd04a66eb90b6ae6c63a9340
Maximus7474 <the.maximus.7474@gmail.com>
2026-01-22 23:03:48 +0100

    feat(server/framework): initialize group permissions on resource start

---
+++ src/server/framework/index.ts
@@ -0,0 +1,18 @@
+import { Ox } from '@communityox/ox_core/server';
+import { Config, Permissions } from '@common/index';
+
+Config.policeGroups.forEach((group) => {
+  Ox.SetGroupPermission(group, 1, 'mdt.access', 'allow');
+
+  for (const [permission, required] of Object.entries(Permissions)) {
+    let grade: number | false = false;
+
+    if (typeof required === 'number') {
+      grade = required;
+    } else {
+      grade = (required as Record<string, number>)[group] ?? false;
+    }
+
+    if (typeof grade === 'number') Ox.SetGroupPermission(group, grade, `mdt.${permission}`, 'allow');
+  }
+});
