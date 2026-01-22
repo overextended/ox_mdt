6f9d73d807e416e89eb738ed30ffb2cf395cef19
Maximus7474 <the.maximus.7474@gmail.com>
2026-01-22 20:58:18 +0100

    feat(typings): mdt types & interfaces

---
+++ src/common/typings/mdt.ts
@@ -0,0 +1,157 @@
+export interface PartialProfileData {
+  firstName: string;
+  lastName: string;
+  dob: number;
+  stateId: string;
+  image?: string;
+}
+
+export interface Profile extends PartialProfileData {
+  charid: number | string;
+  notes?: string;
+  licenses?: Record<string, { label: string } | string>[];
+  vehicles?: { label: string; plate: string }[];
+  pastCharges?: { label: string; count: number }[];
+  relatedReports?: { title: string; author: string; date: string; id: number }[];
+}
+
+export interface Officer {
+  firstName: string;
+  lastName: string;
+  stateId: string;
+  callSign?: string;
+  unitId?: string;
+  position?: [number, number];
+  ped: number;
+  playerId: number;
+  grade: number;
+  group: string;
+}
+
+export interface CriminalProfile extends PartialProfileData {}
+
+export interface Criminal extends CriminalProfile {
+  charges: SelectedCharge[];
+  issueWarrant: boolean;
+  pleadedGuilty?: boolean;
+  processed?: boolean;
+  warrantExpiry?: string;
+  penalty: { time: number; fine: number; reduction?: number };
+}
+
+export interface Charge {
+  label: string;
+  type: 'misdemeanour' | 'felony' | 'infraction';
+  description: string;
+  time: number;
+  fine: number;
+  count: number;
+}
+
+export interface SelectedCharge {
+  label: string;
+  count: number;
+  time: number;
+  fine: number;
+}
+
+export interface Evidence {
+  label: string;
+  image: string;
+}
+
+export interface Report {
+  title: string;
+  id: number;
+  description?: string;
+  officersInvolved: { name: string; callSign: string };
+  evidence: Evidence[];
+  criminals: Criminal[];
+}
+
+export interface PartialReportData {
+  title: string;
+  author: string;
+  date: string;
+  id: number;
+}
+
+export interface Announcement {
+  id: number;
+  contents: string;
+  createdAt: string;
+  firstName: string;
+  lastName: string;
+  creator: string;
+}
+
+export type UnitType = 'car' | 'motor' | 'heli' | 'boat';
+
+export interface Unit {
+  id: number;
+  name: string;
+  members: Officer[];
+  type: UnitType;
+}
+
+export type Units = Unit[];
+
+export interface CallInfo {
+  plate?: string;
+  vehicle?: string;
+}
+
+export interface Call {
+  id: number;
+  offense: string;
+  code: string;
+  completed: boolean;
+  coords: [number, number];
+  blip: number;
+  units: Units;
+  time: number;
+  location: string;
+  isEmergency?: boolean;
+  info: Record<string, string | number>;
+}
+
+export type Calls = Call[];
+
+export interface CallDataInfo {
+  plate?: string;
+  vehicle?: string;
+}
+
+export interface CallData {
+  offense: string;
+  code: string;
+  coords: [number, number];
+  info: CallDataInfo;
+  blip: number;
+}
+
+export type FetchOfficers = {
+  firstName: string;
+  lastName: string;
+  stateId: string;
+}[];
+
+export type FetchCriminals = {
+  stateId: string;
+  firstName: string;
+  lastName: string;
+  reduction: number;
+  warrantExpiry?: string;
+  processed?: number | boolean;
+  pleadedGuilty?: number | boolean;
+  issueWarrant?: boolean;
+  [key: string]: any;
+}[];
+
+export type FetchCharges = {
+  stateId: string;
+  label: string;
+  time?: number;
+  fine?: number;
+  count: number;
+}[];
