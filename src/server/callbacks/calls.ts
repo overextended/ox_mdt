720606bc3f97d4b58ab13897aa09d4f87b220a98
Maximus7474 <the.maximus.7474@gmail.com>
2026-01-23 23:16:22 +0100

    feat(server/callbacks): call related callbacks

---
+++ src/server/callbacks/calls.ts
@@ -0,0 +1,39 @@
+import { CallManager } from '../managers/callManager';
+import { OfficerManager } from '../managers/officerManager';
+import { registerAuthorisedCallback } from '../utils/callback';
+
+registerAuthorisedCallback('ox_mdt:getCalls', (source, type: 'active' | 'completed') => {
+  return CallManager.getCalls(type);
+});
+
+registerAuthorisedCallback('ox_mdt:attachToCall', (source, callId: number) => {
+  const unitId = Player(source).state.mdtUnitId;
+
+  if (!unitId) return false;
+
+  return CallManager.addUnitToCall(callId, unitId);
+});
+
+registerAuthorisedCallback('ox_mdt:detachFromCall', (source, callId: number) => {
+  const unitId = Player(source).state.mdtUnitId;
+
+  if (!unitId) return false;
+
+  return CallManager.removeUnitFromCall(callId, unitId);
+});
+
+registerAuthorisedCallback('ox_mdt:completeCall', (source, callId: number) => {
+  return CallManager.markCallComplete(callId);
+}, 'mark_call_completed');
+
+type SetCallUnitsData = {
+  id: number; // callId
+  units: string[];
+};
+registerAuthorisedCallback('ox_mdt:setCallUnits', (source, data: SetCallUnitsData) => {
+  const officer = OfficerManager.get(source);
+
+  if (!officer || officer.group !== 'dispatch') return false;
+
+  return CallManager.setUnitsOnCall(data.id, data.units);
+});
