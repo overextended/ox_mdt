95dce87e82f664eec95a8344d34ee1a5b694a7c9
Maximus7474 <the.maximus.7474@gmail.com>
2026-01-23 22:04:27 +0100

    feat(server/unitManager): added create & update unit type methods

--- src/server/unitManager.ts
+++ src/server/unitManager.ts
@@ -1,8 +1,33 @@
-import { Officer, Units } from '@common/typings';
+import { Officer, Unit, Units, UnitType } from '@common/typings';
 import { OfficerManager } from './officerManager';
 
 export class UnitManager {
   private static units: Units = {};
+
+  public static createUnit(unitId: string, unitName: string, unitType: UnitType) {
+    const unitData: Unit = {
+      id: unitId,
+      name: unitName,
+      members: [],
+      type: unitType,
+    };
+
+    this.units[unitId] = unitData;
+
+    return unitData;
+  }
+
+  public static updateUnitType(unitId: string, unitType: UnitType): boolean {
+    const unit = this.units[unitId];
+
+    if (!unit) return false;
+
+    unit.type = unitType;
+
+    OfficerManager.triggerEvent('ox_mdt:refreshUnits', this.units);
+
+    return true;
+  }
 
   public static removePlayerFromUnit(officer: Officer, state: StateBagInterface): boolean {
     const unitId = state.mdtUnitId as number;
