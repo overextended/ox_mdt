6e70d80bc15bd50521979c3162682a6b7fb2c30c
Maximus7474 <the.maximus.7474@gmail.com>
2026-01-24 23:21:43 +0100

    feat(server/callbacks): report related callbacks

---
+++ src/server/callbacks/reports.ts
@@ -0,0 +1,167 @@
+import { Criminal, Evidence, PartialReportData } from '@common/typings';
+import { DB } from '../framework/db';
+import { OfficerManager } from '../managers/officerManager';
+import { registerAuthorisedCallback } from '../utils/callback';
+
+registerAuthorisedCallback('ox_mdt:getCriminalProfiles', async (source, search) => {
+  return await DB.getCharacters([search], true);
+});
+
+registerAuthorisedCallback(
+  'ox_mdt:createReport',
+  async (source, title: string) => {
+    const officer = OfficerManager.get(source);
+
+    if (!officer) return;
+
+    return await DB.createReport(title, `${officer.firstName} ${officer.lastName}`);
+  },
+  'create_report'
+);
+
+registerAuthorisedCallback(
+  'ox_mdt:getReports',
+  async (
+    source,
+    data: {
+      search: string | number;
+      page: number;
+    }
+  ) => {
+    let reports: PartialReportData[];
+
+    if (typeof data.search === 'number') {
+      reports = await DB.getReportById(data.search);
+    } else {
+      reports = await DB.selectReports(data.page, data.search);
+    }
+
+    return {
+      hasMore: reports.length === 10,
+      reports,
+    };
+  }
+);
+
+registerAuthorisedCallback('ox_mdt:getReport', async (source, reportId: number) => {
+  let report = await DB.getReportById(reportId);
+
+  if (!report) return;
+
+  const officersInvolved = await DB.getOfficersInvolved(reportId);
+  const evidence = await DB.selectEvidence(reportId);
+  const criminals = await DB.selectCriminalsInvolved(reportId);
+
+  return {
+    ...report,
+    officersInvolved,
+    evidence,
+    criminals,
+  };
+});
+
+registerAuthorisedCallback(
+  'ox_mdt:deleteReport',
+  async (source, reportId) => {
+    return await DB.deleteReport(reportId);
+  },
+  'delete_report'
+);
+
+registerAuthorisedCallback(
+  'ox_mdt:setReportTitle',
+  async (
+    source,
+    data: {
+      id: number;
+      title: string;
+    }
+  ) => {
+    return await DB.updateReportTitle(data.id, data.title);
+  },
+  'edit_report_title'
+);
+
+registerAuthorisedCallback(
+  'ox_mdt:saveReportContents',
+  async (source, reportId) => {
+    return DB.deleteReport(reportId);
+  },
+  'edit_report_contents'
+);
+
+registerAuthorisedCallback(
+  'ox_mdt:addCriminal',
+  async (
+    source,
+    data: {
+      id: number;
+      criminalId: string;
+    }
+  ) => {
+    return await DB.addCriminal(data.id, data.criminalId);
+  },
+  'add_criminal'
+);
+
+registerAuthorisedCallback(
+  'ox_mdt:removeCriminal',
+  async (
+    source,
+    data: {
+      id: number;
+      criminalId: string;
+    }
+  ) => {
+    return await DB.removeCriminal(data.id, data.criminalId);
+  },
+  'remove_criminal'
+);
+
+registerAuthorisedCallback(
+  'ox_mdt:saveCriminal',
+  async (
+    source,
+    data: {
+      id: number;
+      criminal: Criminal;
+    }
+  ) => {
+    if (data.criminal.issueWarrant) {
+      await DB.createWarrant(data.id, data.criminal.stateId, data.criminal.warrantExpiry);
+    } else {
+      await DB.removeWarrant(data.id, data.criminal.stateId);
+    }
+
+    return await DB.saveCriminal(data.id, data.criminal);
+  },
+  'save_criminal'
+);
+
+registerAuthorisedCallback(
+  'ox_mdt:addEvidence',
+  async (
+    source,
+    data: {
+      id: number;
+      evidence: Evidence;
+    }
+  ) => {
+    return await DB.addEvidence(data.id, data.evidence.label, data.evidence.image);
+  },
+  'add_evidence'
+);
+
+registerAuthorisedCallback(
+  'ox_mdt:removeEvidence',
+  async (
+    source,
+    data: {
+      id: number;
+      evidence: Evidence;
+    }
+  ) => {
+    return await DB.removeEvidence(data.id, data.evidence.label, data.evidence.image);
+  },
+  'add_evidence'
+);
