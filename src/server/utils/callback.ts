50b77da07538f863e6d18ae443e2ac75321bfd83
Maximus7474 <the.maximus.7474@gmail.com>
2026-01-24 17:08:11 +0100

    refactor(server/utils): use PermissionKeys type for callback

--- src/server/utils/callback.ts
+++ src/server/utils/callback.ts
@@ -1,10 +1,11 @@
 import { onClientCallback } from '@communityox/ox_lib/server';
 import { isAuthorised } from '../framework/authorizedCheck';
+import { PermissionKeys } from '@common/index';
 
 export const registerAuthorisedCallback = (
   event: string,
   cb: (source: number, ...args: any[]) => any,
-  permission: string | false = 'access'
+  permission: PermissionKeys | 'access' | false = 'access'
 ) => {
   onClientCallback(event, (playerId, ...args) => {
     if (permission !== false && isAuthorised(source, permission)) return false;
