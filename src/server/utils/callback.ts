2381017e0e385a3d131f0304208c14bd5491d286
Maximus7474 <the.maximus.7474@gmail.com>
2026-01-25 22:03:11 +0100

    tweak(server/utils): added dev log if no permission

--- src/server/utils/callback.ts
+++ src/server/utils/callback.ts
@@ -8,7 +8,10 @@ export const registerAuthorisedCallback = (
   permission: PermissionKeys | 'access' | false = 'access'
 ) => {
   onClientCallback(event, (playerId, ...args) => {
-    if (typeof permission === 'string' && !isAuthorised(source, permission)) return false;
+    if (typeof permission === 'string' && !isAuthorised(source, permission)) {
+      DEV: console.info(`User (${source}) tried accessing callback without required permission (${permission}).`);
+      return false;
+    }
 
     return cb(playerId, ...args);
   });
