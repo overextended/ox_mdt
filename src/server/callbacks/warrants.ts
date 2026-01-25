dab1905102f2d1cefdaa77b29b5b6b018065825d
Maximus7474 <the.maximus.7474@gmail.com>
2026-01-25 15:21:17 +0100

    feat(server/callbacks): warrants module

---
+++ src/server/callbacks/warrants.ts
@@ -0,0 +1,21 @@
+import { Charge } from "@common/typings";
+import { registerAuthorisedCallback } from "../utils/callback";
+import { DB } from "../framework/db";
+
+registerAuthorisedCallback('ox_mdt:getRecommendedWarrantExpiry', (source, charges: Charge[]) => {
+  const currentTime = Date.now();
+  const baseWarrantDuration = 259200000;
+  let addonTime = 0;
+
+  for (const charge of charges) {
+    if (charge.time !== 0) {
+      addonTime += (charge.time * 60 * 60000 * charge.count);
+    }
+  }
+
+  return currentTime + addonTime + baseWarrantDuration;
+});
+
+registerAuthorisedCallback('ox_mdt:getWarrants', async (source, search: string) => {
+  return await DB.selectWarrants(search);
+});
