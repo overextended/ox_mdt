ac7a40585f875473375320699cfa694b8fb1d2e8
Maximus7474 <the.maximus.7474@gmail.com>
2026-01-25 18:37:58 +0100

    chore(server): move charge types to typings

--- src/server/callbacks/charges.ts
+++ src/server/callbacks/charges.ts
@@ -1,34 +1,11 @@
 import { oxmysql } from '@communityox/oxmysql';
 import { registerAuthorisedCallback } from '../utils/callback';
-
-const chargeCategories: Record<string, string> = {
-  'OFFENSES AGAINST PERSONS': 'Offenses Against Persons',
-  'OFFENSES INVOLVING THEFT': 'Offenses Involving Theft',
-  'OFFENSES INVOLVING FRAUD': 'Offenses Involving Fraud',
-  'OFFENSES INVOLVING DAMAGE TO PROPERTY': 'Offenses Involving Damage To Property',
-  'OFFENSES AGAINST PUBLIC ADMINISTRATION': 'Offenses Against Public Administration',
-  'OFFENSES AGAINST PUBLIC ORDER': 'Offenses Against Public Order',
-  'OFFENSES AGAINST HEALTH AND MORALS': 'Offenses Agaisnt Health And Morals',
-  'OFFENSES AGAINST PUBLIC SAFETY': 'Offenses Against Public Safety',
-  'OFFENSES INVOLVING THE OPERATION OF A VEHICLE': 'Offenses Involving The Operation Of A Vehicle',
-  'OFFENSES INVOLVING THE WELL-BEING OF WILDLIFE': 'Offenses Involving The Well-Being Of Wildlife',
-};
-
-type ChargeCategoryKey = keyof typeof chargeCategories;
+import { CHARGE_CATEGORIES, ChargeCategoryKey, DbCharge } from '@common/typings';
 
 const charges: Record<ChargeCategoryKey, DbCharge[]> = {};
 
-for (let category of Object.keys(chargeCategories)) {
+for (let category of Object.keys(CHARGE_CATEGORIES)) {
   charges[category] = [];
-}
-
-interface DbCharge {
-  label: string;
-  type: 'misdemeanor' | 'felony' | 'infraction';
-  category: ChargeCategoryKey;
-  description: string;
-  time: number;
-  fine: number;
 }
 
 oxmysql.ready(async () => {
