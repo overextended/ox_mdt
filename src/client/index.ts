826065ab0781540d980d614a89583dcfa296f52f
Maximus7474 <the.maximus.7474@gmail.com>
2026-01-25 18:47:31 +0100

    feat(client): keybinds to interact with the script -> why would we forget these ?

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,1 +1,40 @@
+import { addKeybind, cache } from '@communityox/ox_lib/client';
+import { MdtUiState } from './nui';
 import './nui';
+
+addKeybind({
+  defaultKey: 'm',
+  description: 'Open police MDT',
+  name: 'openMDT',
+  onPressed: MdtUiState.openMdt,
+});
+
+let callsAreFocused: false | CitizenTimer = false;
+addKeybind({
+  defaultKey: 'GRAVE',
+  description: 'Toggle MDT call focus',
+  name: 'focusCalls',
+  onPressed: () => {
+    if (callsAreFocused !== false) {
+      clearInterval(callsAreFocused);
+      callsAreFocused = false;
+      SetNuiFocus(false, false);
+      SetNuiFocusKeepInput(false);
+      return;
+    }
+
+    if (IsNuiFocused() || IsPauseMenuActive()) return;
+
+    SetNuiFocus(true, true);
+    SetNuiFocusKeepInput(true);
+    SetCursorLocation(0.5, 0.5);
+
+    callsAreFocused = setInterval(() => {
+      DisablePlayerFiring(cache.playerId, true);
+      DisableControlAction(0, 1, true);
+      DisableControlAction(0, 2, true);
+      DisableControlAction(2, 199, true);
+      DisableControlAction(2, 200, true);
+    }, 1);
+  },
+});
