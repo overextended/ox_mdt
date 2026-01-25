8b077bd08113960b6f81cdef1d3a3e0e5c4800bd
Maximus7474 <the.maximus.7474@gmail.com>
2026-01-25 16:58:31 +0100

    feat(server/callbacks): added hire, fire & set grade officer callbacks

--- src/server/callbacks/officers.ts
+++ src/server/callbacks/officers.ts
@@ -1,6 +1,10 @@
+import { GetPlayer, GetPlayerFromFilter, Ox } from '@communityox/ox_core/server';
 import { DB } from '../framework';
 import { OfficerManager } from '../managers/officerManager';
 import { registerAuthorisedCallback } from '../utils/callback';
+import { oxmysql } from '@communityox/oxmysql';
+import { stat } from 'fs';
+import { Config } from '@common/index';
 
 registerAuthorisedCallback('ox_mdt:getSearchOfficers', async (source, data: string) => {
   return await DB.searchOfficers(data);
@@ -22,4 +26,109 @@ registerAuthorisedCallback(
     return true;
   },
   'set_call_sign'
+);
+
+registerAuthorisedCallback(
+  'ox_mdt:setOfficerRank',
+  async (
+    source,
+    data: {
+      stateId: string;
+      group: string;
+      grade: number;
+    }
+  ) => {
+    const player = GetPlayer(source);
+    const grade = player.getGroup(data.group);
+
+    if (player.stateId !== data.stateId) return false;
+    if (!grade || grade <= data.grade) return false;
+
+    const target = GetPlayerFromFilter({
+      stateId: data.stateId,
+      groups: data.group,
+    });
+
+    if (target) {
+      if (!target.getGroup(data.group)) return false;
+
+      return target.setGroup(data.group, data.grade + 1);
+    } else {
+      await oxmysql.prepare(
+        `
+      UPDATE character_groups
+      SET grade = ?
+
+      WHERE charId = (
+        SELECT charId
+        FROM characters
+        WHERE stateId = ?
+      ) AND name = ? `,
+        [data.grade + 1, data.stateId, data.group]
+      );
+
+      return true;
+    }
+  },
+  'set_officer_rank'
+);
+
+registerAuthorisedCallback(
+  'ox_mdt:fireOfficer',
+  async (source, stateId: string) => {
+    const target = GetPlayerFromFilter({ stateId });
+
+    if (target) {
+      Config.policeGroups.forEach((group) => {
+        target.setGroup(group, 0);
+      });
+
+      return true;
+    } else {
+      const charId = await oxmysql.prepare('SELECT charid FROM characters WHERE stateId = ?', [stateId]);
+
+      if (!charId) return false;
+
+      await oxmysql.prepare(
+        `
+      DELETE FROM character_groups
+      WHERE charId = ? AND name IN (?)`,
+        [charId, Config.policeGroups]
+      );
+
+      return true;
+    }
+  },
+  'fire_officer'
+);
+
+registerAuthorisedCallback(
+  'ox_mdt:hireOfficer',
+  async (source, stateId: string) => {
+    const target = GetPlayerFromFilter({ stateId });
+
+    if (target) {
+      if (target.getGroup(Config.policeGroups)) return false;
+
+      target.setGroup('police', 1);
+      return true;
+    } else {
+      const charId = await oxmysql.prepare('SELECT charid FROM characters WHERE stateId = ?', [stateId]);
+
+      if (!charId) return false;
+
+      try {
+        await oxmysql.prepare('INSERT INTO `character_groups` (`charId`, `name`, `grade`) VALUES (?, ?, ?)', [
+          charId,
+          'police',
+          1,
+        ]);
+
+        return true;
+      } catch {
+        return false;
+      }
+    }
+  },
+  'hire_officer'
 );
