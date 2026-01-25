9c4a8f0b6ad3825b650badcf8e705d7b0e34e56a
Maximus7474 <the.maximus.7474@gmail.com>
2026-01-25 17:23:57 +0100

    refactor(client): move all server callbacks to separate file

--- src/client/utils.ts
+++ src/client/utils.ts
@@ -1,17 +1,3 @@
-import { triggerServerCallback } from '@communityox/ox_lib/client';
-
-export const serverNuiCallback = <T = any>(event: string, clientCb?: (data: T, cb: (data: T) => void) => void) => {
-  RegisterNuiCallback(event, async function (data: T, cb: (data: T) => void) {
-    const response = (await triggerServerCallback<Promise<unknown>>(`ox_mdt:${event}`, null, data)) as T;
-
-    if (clientCb) {
-      clientCb(response, cb);
-    } else {
-      cb(response);
-    }
-  });
-};
-
 export function SendTypedNUIMessage<T = any>(action: string, data: T) {
   SendNUIMessage({
     action,
