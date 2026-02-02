2417f023b63e8a24121bb3447358a9ed086c23a5
Maximus7474 <the.maximus.7474@gmail.com>
2026-02-02 17:52:42 +0100

    fix(server/utils): added an empty object fallback response

--- src/server/utils/callback.ts
+++ src/server/utils/callback.ts
@@ -7,12 +7,14 @@ export const registerAuthorisedCallback = (
   cb: (source: number, ...args: any[]) => any,
   permission: PermissionKeys | 'access' | false = 'access'
 ) => {
-  onClientCallback(event, (playerId, ...args) => {
+  onClientCallback(event, async (playerId, ...args) => {
     if (typeof permission === 'string' && !isAuthorised(source, permission)) {
       DEV: console.info(`User (${source}) tried accessing callback without required permission (${permission}).`);
       return false;
     }
 
-    return cb(playerId, ...args);
+    const response = await cb(playerId, ...args);
+
+    return response ?? {};
   });
 };
